cbbccf4a5a0f9f3bcfc8f2d168aaee4bc441c899
@k$hay <akshaynaik5787@gmail.com>
2023-12-09 12:03:52 +0530

    fixed a bug

--- Frontend/src/components/DashboardItems.jsx
+++ Frontend/src/components/DashboardItems.jsx
@@ -13,109 +13,81 @@ export default function DashboardItems() {
 
   useEffect(() => {
     if (!product) {
+      console.log("loaded");
       getProducts().then((data) => {
         dispatch(setAllProducts(data));
       });
     }
-  }, [product]);
+    else {
+      console.log("loaded");
+      getProducts().then((data) => {
+        dispatch(setAllProducts(data));
+      });
+    }
+  }, []);
 
   return (
     <>
-      {!product ? (
-        <>
-          <div className={styles.dbItemsContainer}>
-            <DataTable
-              columns={[
-                {
-                  title: "Image",
-                  field: "product_image",
-                },
-                {
-                  title: "Name",
-                  field: "product_name",
-                },
-                {
-                  title: "Category",
-                  field: "product_category",
-                },
-                {
-                  title: "Price",
-                  field: "product_price",
-                  render: (rowData) => (
-                    <p>{parseFloat(rowData.product_price).toFixed(2)}</p>
-                  ),
-                },
-              ]}
-              title="Product List"
-            />
-          </div>
-        </>
-      ) : (
-        <>
-          <div className={styles.dbItemsContainer}>
-            <DataTable
-              columns={[
-                {
-                  title: "Image",
-                  field: "product_image",
-                  render: (rowData) => (
-                    <div className={styles.imgWrapper}>
-                      <img src={rowData.product_image} />
-                    </div>
-                  ),
-                },
-                {
-                  title: "Name",
-                  field: "product_name",
-                },
-                {
-                  title: "Category",
-                  field: "product_category",
-                },
-                {
-                  title: "Price",
-                  field: "product_price",
-                  render: (rowData) => (
-                    <p className={styles.priceWrapper}>
-                      <LiaRupeeSignSolid />
-                      {parseFloat(rowData.product_price).toFixed(2)}
-                    </p>
-                  ),
-                },
-              ]}
-              data={product}
-              title="Product List"
-              actions={[
-                {
-                  icon: "edit",
-                  tooltip: "Edit Data",
-                  onClick: (event, rowData) => {
-                    alert("You want do edit " + rowData.product_id);
-                  },
-                },
-                {
-                  icon: "delete",
-                  tooltip: "Delete Data",
-                  onClick: (event, rowData) => {
-                    if (
-                      window.confirm(
-                        "Are you sure you want to perform this action"
-                      )
-                    ) {
-                      deleteProducts(rowData.product_id).then(() => {
-                        toast.success("Product Deleted");
-                        getProducts().then((data) => {
-                          dispatch(setAllProducts(data));
-                        });
-                      });
-                    }
-                  },
-                },
-              ]}
-            />
-          </div>
-        </>
-      )}
+      <div className={styles.dbItemsContainer}>
+        <DataTable
+          columns={[
+            {
+              title: "Image",
+              field: "product_image",
+              render: (rowData) => (
+                <div className={styles.imgWrapper}>
+                  <img src={rowData.product_image} />
+                </div>
+              ),
+            },
+            {
+              title: "Name",
+              field: "product_name",
+            },
+            {
+              title: "Category",
+              field: "product_category",
+            },
+            {
+              title: "Price",
+              field: "product_price",
+              render: (rowData) => (
+                <p className={styles.priceWrapper}>
+                  <LiaRupeeSignSolid />
+                  {parseFloat(rowData.product_price).toFixed(2)}
+                </p>
+              ),
+            },
+          ]}
+          data={product || []}
+          title="Product List"
+          actions={[
+            {
+              icon: "edit",
+              tooltip: "Edit Data",
+              onClick: (event, rowData) => {
+                alert("You want do edit " + rowData.product_id);
+              },
+            },
+            {
+              icon: "delete",
+              tooltip: "Delete Data",
+              onClick: (event, rowData) => {
+                if (
+                  window.confirm("Are you sure you want to perform this action")
+                ) {
+                  deleteProducts(rowData.product_id).then(() => {
+                    toast.success("Product Deleted");
+                    getProducts().then((data) => {
+                      dispatch(setAllProducts(data));
+                    });
+                  });
+                }
+              },
+            },
+          ]}
+        />
+      </div>
     </>
   );
 }
